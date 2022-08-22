18c5f065d42eec46969177dce6f678a6e120c9cf
saravanan06git <saravanan.p@constient.com>
2022-08-22 11:32:01 +0530

    axis bank design

--- src/components/ApiData.jsx
+++ src/components/ApiData.jsx
@@ -2,30 +2,40 @@ import React from 'react'
 
 const ApiData = () => {
   return (
-    <div className='pt-5 text-center'>
+    <div className='pt-4 text-center'>
         <table className='table table-borderless table-striped'>
             <thead>
-                <tr>
+                <tr className='lh-lg'>
                     <th className='apth text-uppercase '>servers</th>
                     <th className='apth text-uppercase'>cpu</th>
                     <th className='apth text-uppercase'>current users</th>
                 </tr>
             </thead>
             <tbody>
-                    <tr>
-                        <td className='shadow rounded-5'>google</td>
-                        <td className='shadow rounded-5'>100%</td>
-                        <td className='shadow rounded-5'>saro</td>
+                    <tr className='lh-lg'>
+                        <td className='shadow rounded-5'>10.2.2.148</td>
+                        <td className='shadow rounded-5'>80%</td>
+                        <td className='shadow rounded-5'>98</td>
                     </tr>
-                    <tr>
-                        <td className='shadow rounded-5'>cgs</td>
-                        <td className='shadow rounded-5'>100%</td>
-                        <td className='shadow rounded-5'>saro</td>
+                    <tr className='lh-lg'>
+                        <td className='shadow rounded-5'>20.2.2.158</td>
+                        <td className='shadow rounded-5'>90%</td>
+                        <td className='shadow rounded-5'>88</td>
                     </tr>
-                    <tr>
-                        <td className='shadow rounded-5'>google</td>
+                    <tr className='lh-lg'>
+                        <td className='shadow rounded-5'>30.2.2.168</td>
                         <td className='shadow rounded-5'>100%</td>
-                        <td className='shadow rounded-5'>saro</td>
+                        <td className='shadow rounded-5'>75</td>
+                    </tr>
+                    <tr className='lh-lg'>
+                        <td className='shadow rounded-5'>40.2.2.168</td>
+                        <td className='shadow rounded-5'>95%</td>
+                        <td className='shadow rounded-5'>85</td>
+                    </tr>
+                    <tr className='lh-lg'>
+                        <td className='shadow rounded-5'>50.2.2.168</td>
+                        <td className='shadow rounded-5'>100%</td>
+                        <td className='shadow rounded-5'>90</td>
                     </tr>
             </tbody>
         </table>
--- src/components/DataCount.jsx
+++ src/components/DataCount.jsx
@@ -6,11 +6,7 @@ import EsignValue from "./EsignValue";
 
 const DataCount = () => {
   return (
-    <div className="d-flex flex-column text-center pt-5 px-5">
-      <div className="d-flex flex-row justify-content-between pb-4 px-2">
-        <div className="valhead fw-bold text-uppercase shadow rounded-pill px-3">screen</div>
-        <div className="valhead fw-bold text-uppercase shadow rounded-pill px-3">count</div>
-      </div>
+    <div className="d-flex flex-column text-center pt-4 px-5">
       <div className="d-flex flex-row align-items-center">
         <div className="datadiv py-3 col-5 rounded-4 shadow">
           <p className="datahead m-0 fw-bold">Registration</p>
